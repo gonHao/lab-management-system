bab902277030e8ab64cc72bf7b0516006dc9c6ab
xyp <1050509820>
2019-04-04 16:35:42 +0800

    xyp

--- src/main/java/com/kmust/labmanagementsystem/controller/MainController.java
+++ src/main/java/com/kmust/labmanagementsystem/controller/MainController.java
@@ -1,7 +1,9 @@
-package com.kmust.labmanagementsystem.controller;
+package com.kmust.labManagementSystem.controller;
 
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+@Controller
 public class MainController {
     @RequestMapping("/index")
     public String hi() {
