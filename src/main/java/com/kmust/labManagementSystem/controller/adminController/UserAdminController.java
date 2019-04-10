aa01c3d215ef90bc843458b1cc0fc956aa06b9db
gonHao <806450030@qq.com>
2019-04-10 14:26:51 +0800

    加入security框架

--- src/main/java/com/kmust/labManagementSystem/controller/adminController/UserAdminController.java
+++ src/main/java/com/kmust/labManagementSystem/controller/adminController/UserAdminController.java
@@ -1,12 +1,52 @@
 package com.kmust.labManagementSystem.controller.adminController;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Controller
 public class UserAdminController {
+    private Logger logger = LoggerFactory.getLogger(getClass());
+    private RequestCache requestCache = new HttpSessionRequestCache();
+    private RedirectStrategy redirectStrategy =  new DefaultRedirectStrategy();
     @RequestMapping("/login")
     public String login(){
         return "/login.html";
+    }
+
+    /**
+     * 当需要身份认证时，跳转到这里
+     * @param request
+     * @param response
+     * @return
+     */
+    @RequestMapping("/require")
+    @ResponseStatus(code = HttpStatus.UNAUTHORIZED)
+    @ResponseBody
+    public String requireAuthention(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        SavedRequest savedRequest = requestCache.getRequest(request,response);
+        if(savedRequest!=null){
+            String target =savedRequest.getRedirectUrl();
+            if(StringUtils.endsWithIgnoreCase(target,".html")){
+                redirectStrategy.sendRedirect(request,response,"/login");
+
+            }
+        }
+        return null;
     }
 }
